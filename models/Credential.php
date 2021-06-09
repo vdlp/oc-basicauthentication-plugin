42c26da09e875335ab6a51ecb41a6390359fa693
Alwin Drenth <adrenth@gmail.com>
2021-06-09 16:52:51 +0200

    Add console command for adding credentials

--- models/Credential.php
+++ models/Credential.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace Vdlp\BasicAuthentication\Models;
 
-use Eloquent;
 use October\Rain\Database\Model;
-
-/** @noinspection ClassOverridesFieldOfSuperClassInspection */
-/** @noinspection LongInheritanceChainInspection */
 
 /**
  * Class Credential
  *
  * @package Vdlp\BasicAuthentication\Models
- * @mixin Eloquent
  */
 class Credential extends Model
 {
